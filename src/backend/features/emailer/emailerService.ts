267a2592e2fc7f5ce3fe61bfc550358d721d04e8
NathansStuff <n.odonnell@hotmail.com>
2023-06-25 19:11:31 +1000

    feat: error messaging for emailer

--- src/backend/features/emailer/emailerService.ts
+++ src/backend/features/emailer/emailerService.ts
@@ -1,30 +1,31 @@
 import nodemailer from 'nodemailer';
 
-export function sendEmail(name: string, email: string, message: string): void {
-  console.log('name', name);
-  console.log('email', email);
-  console.log('message', message);
-  console.log('Sending email');
-  const user = process.env.EMAIL_USER ?? '';
-  const pass = process.env.EMAIL_PASSWORD ?? '';
-  console.log('user', user);
-  console.log('pass', pass);
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user,
-      pass,
-    },
-  });
+export async function sendEmail(name: string, email: string, message: string): Promise<string> {
+  try {
+    const user = process.env.EMAIL_USER ?? '';
+    const pass = process.env.EMAIL_PASSWORD ?? '';
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user,
+        pass,
+      },
+    });
 
-  // Setup email data
-  transporter.sendMail({
-    from: '"Contact Form Notification" <pslandscaping@gmail.com>',
-    to: process.env.EMAIL_RECIPIENT ?? 'jezza121@hotmail.com',
-    subject: 'Contact Form Notification',
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  });
+    // Setup email data
+    await transporter.sendMail({
+      from: '"Contact Form Notification" <pslandscaping@gmail.com>',
+      to: process.env.EMAIL_RECIPIENT ?? '',
+      subject: 'Contact Form Notification',
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    });
+
+    return 'success';
+  } catch (error) {
+    console.error(error);
+    return `error: ${error}`;
+  }
 }
