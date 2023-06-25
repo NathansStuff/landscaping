6731f82cd933859c4ab93bab653ff559c8d6ba4e
NathansStuff <n.odonnell@hotmail.com>
2023-06-25 17:53:17 +1000

    fix: remove bad eslint

--- src/app/exceptions/ErrorHandler.ts
+++ src/app/exceptions/ErrorHandler.ts
@@ -7,7 +7,6 @@ import { BadRequestError } from './BadRequestError';
 import { CustomError } from './CustomError';
 import { NotFoundError } from './NotFoundError';
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function ErrorHandler(err: CustomError, req: NextApiRequest, res: NextApiResponse) {
   const status = err.status !== null && err.status !== undefined ? err.status : 500;
   const message = err.message.length > 0 ? err.message : 'Something went wrong';
